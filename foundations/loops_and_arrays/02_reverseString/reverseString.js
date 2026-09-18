ac9ecad1aada5b3fd5a4cf53850c126c6ff205b3
Paulo Lopes <133612870+iPauloLopes@users.noreply.github.com>
2026-09-18 15:58:12 -0300

    Complete reverseString exercise

--- foundations/loops_and_arrays/02_reverseString/reverseString.js
+++ foundations/loops_and_arrays/02_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const invertedStr = Array.from(string).reverse().join('')
+    return invertedStr
 };
+
+reverseString('hello')
+reverseString('hello there')
+reverseString('123! abc! Hello, Odinite.')
+reverseString('')
 
 // Do not edit below this line
 module.exports = reverseString;
