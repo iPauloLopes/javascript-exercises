8cfce682dca8b701b99a7c6cbe6d2fd3e8808ec0
Paulo Lopes <133612870+iPauloLopes@users.noreply.github.com>
2026-09-21 17:04:36 -0300

    Complete tempConversion exercise

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) / (9/5) * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * 9/5 + 32) * 10) / 10
 };
+
+convertToCelsius(32)
+convertToCelsius(100)
+convertToCelsius(-100)
+
+convertToFahrenheit(0)
+convertToFahrenheit(73.2)
+convertToFahrenheit(-10)
 
 // Do not edit below this line
 module.exports = {
